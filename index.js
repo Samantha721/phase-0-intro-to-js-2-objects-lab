0fd8e619a4d997b98f4e65b8ae5b6791d95c7498
Samantha Navarro <samantha.m.navarro@gmail.com>
2022-05-18 14:40:37 -0700

    commit tree 890743ea7ac8388ef98eb4762960aa3b7210623a

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+const employee = {
+  name: "Sam",
+  streetAddress: "New York City",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newValue = { ...employee };
+  newValue[key] = value;
+  return newValue;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const removeKey = { ...employee };
+  delete removeKey[key];
+  return removeKey;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
